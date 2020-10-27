005e7de6d1fadd0e06c49970f2232b9d8dd753f6
Vlad <vladikzh@mail.ru>
2020-10-27 23:07:58 +0300

    Add registration and encryption

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, Sequelize) => {
         password: {
             type: Sequelize.STRING,
             allowNull: false
-        }
+        },
+
     }, {
         tableName: 'users'
     });
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+//-------- Константы -----------//
+const SUCCESS = { status: "success" };
+const ERROR = { status: "error" };
+
 //-------- Requires -----------//
 const express = require("express"),
   app = express(),
   server = require("http").createServer(app);
+
+const bcrypt = require("bcrypt");
 
 const PORT = process.env.PORT || 3000;
 
@@ -30,33 +36,33 @@ server.listen(PORT, () => {
     console.error("Unable to connect to the database:", error);
   }
   //Синхронизируем локальные модели с таблицами базы данных
-  await db.sequelize.sync({force: true});
+  await db.sequelize.sync({ force: true });
   console.log("=========Resynced database==========");
 
   createTags();
 
-  const email = "vlad@mail.ru",
-    password = "123456",
-    name = "vlad",
-    surname = "zhavoronkov";
+  // const email = "vlad@mail.ru",
+  //   password = "123456",
+  //   name = "vlad",
+  //   surname = "zhavoronkov";
 
-  await db.user.create(
-    {
-      email: email,
-      password: password,
-      personal: {
-        name: name,
-        surname: surname,
-      },
-      tags: [
-          {name: 'C++'},
-          {name: 'Python'}
-      ]
-    },
-    {
-      include: [db.personal, db.tag]
-    }
-  );
+  //   const salt = await bcrypt.genSalt(10); //async
+  //   const hashPassword = await bcrypt.hash(password, salt); //async
+  // const currentUser = await db.user.create(
+  //   {
+  //     email: email,
+  //     password: hashPassword,
+  //     personal: {
+  //       name: name,
+  //       surname: surname,
+  //     },
+  //   },
+  //   {
+  //     include: db.personal
+  //   }
+  // );
+  // const ntag = await db.tag.findOne({where: {name: 'C++'}});
+  // await currentUser.addTag(ntag);
 })();
 
 async function createTags() {
@@ -68,13 +74,35 @@ async function createTags() {
   await db.tag.create({ name: "PHP" });
 }
 
-/*io.on('connection', client => {
-    client.on('registration', data => {
-        const name = data.name,
-            surname = data.surname,
-            email = data.email,
-            password = data.password;
+io.on("connection", (client) => {
+  client.on("registration", async (data, callBack) => {
+    const name = data.name,
+      surname = data.surname,
+      email = data.email,
+      password = data.password;
 
-        
-    });
-});*/
+
+    //TODO: Insert this to the sequelize 
+    const salt = await bcrypt.genSalt(10); //async
+    const hashPassword = await bcrypt.hash(password, salt); //async
+
+    try {
+      await db.user.create(
+        {
+          email: email,
+          password: hashPassword,
+          personal: {
+            name: name,
+            surname: surname,
+          },
+        },
+        {
+          include: db.personal,
+        }
+      );
+      callBack(JSON.stringify(SUCCESS));
+    } catch (err) {
+      callBack(JSON.stringify(ERROR));
+    }
+  });
+});
